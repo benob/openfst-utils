c5e77328dc548337fb8d24a91a8a9b21ad814774
Benoit Favre <benoit.favre@gmail.com>
2012-02-16 15:49:07 +0100

    working and accurate ngram expander

--- ngram-expand.cc
+++ ngram-expand.cc
@@ -1,5 +1,4 @@
 #include <fst/fstlib.h>
-#include <set>
 #include <tr1/unordered_map>
 #include <list>
 
@@ -8,47 +7,49 @@ using namespace std;
 
 typedef int64 State;
 
-struct Clique {
+struct Context {
     State inputState;
     State outputState;
     list<StdArc> seq;
-    Clique() {}
-    Clique(State _inputState, State _outputState) : inputState(_inputState), outputState(_outputState) {}
-    Clique& operator=(const Clique& other) {
+    size_t length;
+    Context() {}
+    Context(State _inputState, State _outputState, size_t _length) : inputState(_inputState), outputState(_outputState), length(_length) {}
+    Context& operator=(const Context& other) {
         inputState = other.inputState;
+        outputState = other.outputState;
+        length = other.length;
         seq = other.seq;
         return *this;
     }
     void print() {
-        cerr << "state: " << inputState << " labels:";
+        cerr << "state: in=" << inputState << " out=" << outputState << " labels:";
         for(list<StdArc>::const_iterator i = seq.begin(); i != seq.end(); i++) {
             cerr << " " << i->ilabel;
         }
         cerr << endl;
     }
-    void push(const StdArc& arc, int maxsize) {
-        if(seq.size() > 0) {
-            inputState = seq.back().nextstate;
-        }
+    void push(const StdArc& arc) {
         seq.push_back(arc);
-        if((int) seq.size() > maxsize) seq.pop_front();
-    }
-    State next() {
-        return seq.back().nextstate;
+        inputState = arc.nextstate;
+        if(seq.size() > length) seq.pop_front();
     }
     struct LabelsHash {
-        size_t operator()(const Clique& a) const {
+        size_t operator()(const Context& a) const {
             size_t output = a.inputState;
-            for(list<StdArc>::const_iterator i = a.seq.begin(); i != a.seq.end(); i++) output ^= i->ilabel ^ i->olabel;
+            list<StdArc>::const_iterator i = a.seq.begin(); 
+            if(a.seq.size() == a.length) i++;
+            for(; i != a.seq.end(); i++) output ^= i->ilabel ^ i->olabel;
             return output;
         }
     };
     struct LabelsEqual {
-        int operator()(const Clique& a, const Clique& b) const {
+        int operator()(const Context& a, const Context& b) const {
             if(a.inputState != b.inputState) return false;
             if(a.seq.size() != b.seq.size()) return false;
             list<StdArc>::const_iterator i = a.seq.begin();
             list<StdArc>::const_iterator j = b.seq.begin();
+            if(a.seq.size() == a.length) i++;
+            if(b.seq.size() == b.length) j++;
             for(; i != a.seq.end() && j != b.seq.end(); i++, j++) {
                 if(i->ilabel != j->ilabel) return false;
                 if(i->olabel != j->olabel) return false;
@@ -56,34 +57,15 @@ struct Clique {
             return true;
         }
     };
-    struct Compare {
-        bool operator()(const Clique& a, const Clique& b) {
-            if(a.inputState != b.inputState) return a.inputState < b.inputState;
-            if(a.seq.size() != b.seq.size()) return a.seq.size() < b.seq.size();
-            list<StdArc>::const_iterator i = a.seq.begin();
-            list<StdArc>::const_iterator j = b.seq.begin();
-            for(; i != a.seq.end() && j != b.seq.end(); i++, j++) {
-                if(i->ilabel < j->ilabel) return true;
-                if(i->olabel < j->olabel) return true;
-                //if(i->weight < j->weight) return true;
-                if(i->nextstate < j->nextstate) return true;
-            }
-            return false;
-        }
-    };
-    struct CompareLabels {
-        bool operator()(const Clique& a, const Clique& b) {
-            if(a.inputState != b.inputState) return a.inputState < b.inputState;
-            if(a.seq.size() != b.seq.size()) return a.seq.size() < b.seq.size();
-            list<StdArc>::const_iterator i = a.seq.begin();
-            list<StdArc>::const_iterator j = b.seq.begin();
-            for(; i != a.seq.end() && j != b.seq.end(); i++, j++) {
-                if(i->ilabel < j->ilabel) return true;
-                if(i->olabel < j->olabel) return true;
-            }
-            return false;
-        }
-    };
+    StdArc::Weight weight() const {
+        return seq.back().weight;
+    }
+    int64 ilabel() const {
+        return seq.back().ilabel;
+    }
+    int64 olabel() const {
+        return seq.back().ilabel;
+    }
 };
 
 int main(int argc, char** argv) {
@@ -98,53 +80,37 @@ int main(int argc, char** argv) {
     }
     StdVectorFst output;
 
-    unordered_map<Clique, State, Clique::LabelsHash, Clique::LabelsEqual> ngrams;
-    set<Clique, Clique::Compare> visited;
-    list<Clique> queue;
+    unordered_map<Context, State, Context::LabelsHash, Context::LabelsEqual> outputStates;
+    list<Context> queue; // queue all unprocessed contexts
 
-    State startState = 0;
+    State outputStart = 0;
     output.AddState();
-    output.SetStart(startState);
-    for(ArcIterator<StdVectorFst> aiter(*input, input->Start()); !aiter.Done(); aiter.Next()) {
-        const StdArc &arc = aiter.Value();
-        State nextState = output.NumStates();
-        Clique arcs(input->Start(), nextState);
-        arcs.push(arc, ngram_size);
-        queue.push_back(arcs);
-        ngrams[arcs] = nextState;
-        output.AddState();
-        output.AddArc(startState, StdArc(arc.ilabel, arc.olabel, arc.weight, nextState));
-    }
+    output.SetStart(outputStart);
+    queue.push_back(Context(input->Start(), outputStart, ngram_size));
 
     while(queue.size() > 0) {
-        Clique current = queue.front();
-        //current.print();
+        Context current = queue.front();
         queue.pop_front();
-        State inputState = current.next();
+        State inputState = current.inputState;
         State arcStartState = current.outputState;
         for(ArcIterator<StdVectorFst> aiter(*input, inputState); !aiter.Done(); aiter.Next()) {
             const StdArc &arc = aiter.Value();
-            Clique next = current;
-            //next.print();
-            next.push(arc, ngram_size);
-            ////////////////////////////////////////////////////
-            unordered_map<Clique, State>::iterator found = ngrams.find(next);
+            Context next = current;
+            next.push(arc);
+            unordered_map<Context, State>::iterator found = outputStates.find(next);
             int arcEndState = output.NumStates();
-            if(found == ngrams.end()) {
-                ngrams[next] = arcEndState;
+            if(found == outputStates.end()) {
+                outputStates[next] = arcEndState;
                 output.AddState();
+                next.outputState = arcEndState;
+                queue.push_back(next);
             } else {
                 arcEndState = found->second;
             }
-            next.outputState = arcEndState;
             if(input->Final(arc.nextstate) != arc.weight.Zero()) {
                 output.SetFinal(arcEndState, input->Final(arc.nextstate));
             }
-            output.AddArc(arcStartState, StdArc(arc.ilabel, arc.olabel, arc.weight, arcEndState));
-            ////////////////////////////////////////////////////
-            if(visited.insert(next).second == true) {
-                queue.push_back(next);
-            }
+            output.AddArc(arcStartState, StdArc(next.ilabel(), next.olabel(), next.weight(), arcEndState));
         }
     }
     output.SetInputSymbols(input->InputSymbols());
