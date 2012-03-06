77882e26ffea1c83c519f98ccb68be71bb8df151
Benoit Favre <benoit.favre@gmail.com>
2012-03-06 09:04:35 +0100

    simpilfied chained transuducer minimization: does not generate a mapping file anymore

--- fstminimize-transducer.cc
+++ fstminimize-transducer.cc
@@ -1,15 +1,14 @@
 #include <fst/fstlib.h>
-#include <fst/script/fstscript.h>
 
 using namespace fst;
-using namespace fst::script;
+
 int main(int argc, char** argv) {
-    FstClass *ifst = FstClass::Read("");
-    VectorFstClass ofst(ifst->ArcType());
-    RmEpsilon(*ifst, &ofst);
-    Encode(&ofst, kEncodeLabels|kEncodeWeights, false, "tmp.encoded");
-    Determinize(ofst, &ofst);
-    Minimize(&ofst, &ofst);
-    Decode(&ofst, "tmp.encoded");
-    ofst.Write("");
+    EncodeMapper<StdArc> mapper(kEncodeLabels, ENCODE);
+    StdVectorFst *ifst = StdVectorFst::Read("");
+    RmEpsilon(ifst);
+    Encode(ifst, &mapper);
+    Determinize(*ifst, ifst);
+    Minimize(ifst);
+    Decode(ifst, mapper);
+    ifst->Write("");
 }
