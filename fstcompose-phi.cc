5b1eda6e65ba07b8e95bf2c85fdfd021317f983f
Benoit Favre <benoit.favre@gmail.com>
2012-05-29 18:00:10 +0200

    some fixes for fstcompose-phi; not functional yet

--- fstcompose-phi.cc
+++ fstcompose-phi.cc
@@ -21,19 +21,30 @@ using namespace fst;
 typedef PhiMatcher< SortedMatcher<StdFst> > PM;
 
 int main(int argc, char** argv) {
-    if(argc != 2) {
+    if(argc != 3) {
         cerr << "usage: cat <input1> | " << argv[0] << " <phi-symbol> <input2>\n";
         return 1;
     }
-    int phiLabel = atoi(argv[1]);
     StdVectorFst* input1 = StdVectorFst::Read(argv[2]);
     StdVectorFst* input2 = StdVectorFst::Read("");
+    bool relabel;
+    SymbolTable *symbolMap = MergeSymbolTable(*(input1->OutputSymbols()), *(input2->InputSymbols()), &relabel);
+    Relabel(input1, NULL, symbolMap);
+    Relabel(input2, symbolMap, NULL);
+    input1->SetOutputSymbols(symbolMap);
+    ArcSort(input1, StdOLabelCompare());
+    input2->SetInputSymbols(symbolMap);
+    int64 phiLabel = symbolMap->AddSymbol(argv[1]);
+    std::cerr << "phi = " << phiLabel << " " << symbolMap->Find(phiLabel) << "\n";
     ArcSort(input1, OLabelCompare<StdArc>()); 
     ComposeFstOptions<StdArc, PM> opts;
     opts.gc_limit = 0;
     opts.matcher1 = new PM(*input1, MATCH_OUTPUT, phiLabel);
     opts.matcher2 = new PM(*input2, MATCH_NONE, kNoLabel);
     // composition
-    ComposeFst<StdArc> output(*input1, *input2, opts);
+    ComposeFst<StdArc> composed(*input1, *input2, opts);
+    StdVectorFst output(composed);
+    Connect(&output);
     output.Write("");
+    delete symbolMap;
 }
